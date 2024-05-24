1a98c3e280ebd651f36b3b7c481707d32a017e39
Samuel <samisole1996@gmail.com>
2024-05-24 07:52:05 -0400

    Testing github actions

--- service/routes.py
+++ service/routes.py
@@ -141,7 +141,7 @@ def check_content_type(media_type):
         f"Content-Type must be {media_type}",
     )
 
-def test_method_not_allowed(self):
-    """It should not allow an illegal method call"""
-    resp = self.client.delete(BASE_URL)
-    self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+# def test_method_not_allowed(self):
+#     """It should not allow an illegal method call"""
+#     resp = self.client.delete(BASE_URL)
+#     self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
